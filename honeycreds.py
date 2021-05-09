b19362747b75cd4503228e28bf813d8c81e96289
Ben0xA <web@ben0xa.com>
2021-05-09 15:34:41 -0400

    Added support for Splunk> event forwarding.

--- honeycreds.py
+++ honeycreds.py
@@ -3,13 +3,14 @@
 
 # Author: Ben Ten (Ben0xA)
 # HoneyCreds - Detecting LLMNR/NBNS/HTTP Listeners
-# Updated: 10/29/2020
-# Version: 0.1
+# Updated: 5/9/2021
+# Version: 0.2
 
 # Requires:
 # python 3
 # smbprotocol
 # cffi
+# splunk-sdk
 
 import smbclient
 import os
@@ -18,7 +19,10 @@ import logging
 import time
 import sys
 import requests
+import socket
 import threading
+import splunklib.client as client
+from datetime import datetime
 from signal import signal, SIGINT
 
 # --------- SETTINGS ----------
@@ -29,13 +33,13 @@ from signal import signal, SIGINT
 def_username = 'honeycreds' 
 
 #This can match your current Short Domain
-def_domain   = 'XQQX'
+def_domain   = 'TSDEV'
 
 #Make this whatever you want. Note: HTTP requests will send this in plaintext
 def_password = 'This is a honey cred account.'
 
 #The FQDN. Leave .local at the end.
-def_fqdn     = 'xqqx.local'
+def_fqdn     = 'ts-dev-mx.local'
 
 #The hostname that DOES NOT EXIST but looks legit.
 def_hostname = 'HNECRD01'
@@ -43,7 +47,7 @@ def_hostname = 'HNECRD01'
 #The log file and location
 def_logfile  = 'honeycreds.log'
 
-#bility to turn SMB or HTTP on or off. Set to "OFF" to turn off.
+#Ability to turn SMB or HTTP on or off. Set to "OFF" to turn off.
 SMB = 'ON'
 HTTP = 'ON'
 
@@ -51,12 +55,27 @@ HTTP = 'ON'
 SMB_SLEEP = 5
 HTTP_SLEEP = 12
 
+#Forwarders
+SPLUNK = 'ON'
+ELK = 'OFF' #Coming Soon
+
+#Splunk Forwarding
+SPLUNK_HOSTNAME = 'localhost'
+SPLUNK_PORT = 8089
+SPLUNK_USERNAME = 'admin'
+SPLUNK_PASSWORD = None
+SPLUNK_TOKEN = None
+SPLUNK_INDEX = 'honeycreds'
+
 # --------- STOP ----------
 # Do not change anything below this line.
 smb_Thread = None
 http_Thread = None
 smb_exit = threading.Event()
 http_exit = threading.Event()
+splunk_service = None
+splunk_index = None
+local_hostname = socket.gethostname()
 
 def signal_handler(sig, frame):	
 	global smb_Thread, http_Thread, exit
@@ -83,6 +102,20 @@ def init():
 	)
 	SMB = str.upper(SMB)
 	HTTP = str.upper(HTTP)
+
+def init_splunk():
+	global splunk_service, splunk_index
+	global SPLUNK_HOSTNAME,SPLUNK_PORT, SPLUNK_USERNAME, SPLUNK_PASSWORD, SPLUNK_TOKEN, SPLUNK_INDEX	
+	if SPLUNK_TOKEN != None:
+		splunk_service = client.connect(host=SPLUNK_HOSTNAME, port=SPLUNK_PORT, splunkToken=SPLUNK_TOKEN)
+	else:
+		splunk_service = client.connect(host=SPLUNK_HOSTNAME, port=SPLUNK_PORT, username=SPLUNK_USERNAME, password=SPLUNK_PASSWORD)
+
+	#Get or create index
+	try:
+		splunk_index = splunk_service.indexes[SPLUNK_INDEX]
+	except:
+		splunk_index = splunk_service.indexes.create(SPLUNK_INDEX)
 
 def banner():
 	oncolor = termcolor.GREEN
@@ -142,7 +175,7 @@ class SMBClient(threading.Thread):
 		self.hostname = hostname
 
 	def run(self):
-		global def_password, smb_exit
+		global def_password, smb_exit, SPLUNK, splunk_index, local_hostname
 		username = self.username
 		hostname = self.hostname
 		while smb_exit.is_set() == False:
@@ -153,7 +186,10 @@ class SMBClient(threading.Thread):
 					connected = True
 			except Exception as exception:
 				if type(exception).__name__ == 'AccessDenied':
-					drslt = subprocess.check_output('dig +short ' + hostname, shell=True, text=True)
+					try:
+						drslt = subprocess.check_output('dig +short ' + hostname, shell=True).decode('utf-8')
+					except:
+						pass
 					drslt_parts = drslt.split('\n')
 					rmt_ip = 'Unknown'
 					if len(drslt_parts) > 1:
@@ -161,6 +197,15 @@ class SMBClient(threading.Thread):
 							rmt_ip = drslt_parts[1]
 						else:
 							rmt_ip = drslt_parts[0]
+
+					if SPLUNK:
+						event = str(time.time()) + ','
+						event += 'protocol="SMB",'
+						event += 'ip_address=' + rmt_ip + ','
+						event += 'honey_hostname="' + hostname + '",'
+						event += 'honey_username="' + username + '",'
+						event += 'message="Responder activity detected!"'
+						splunk_index.submit(event, sourcetype="honeycreds.service", host=local_hostname)
 
 					logging.critical(messages.RSP_RECVD % {'color':'', 'bold':'', 'proto':'SMB', 'end':'', 'ip':rmt_ip, 'hostname':hostname})
 					print(messages.RSP_RECVD % {'color':termcolor.BLUE, 'bold':termcolor.BOLD, 'proto':'SMB', 'end':termcolor.END, 'ip':rmt_ip, 'hostname':hostname})
@@ -177,14 +222,17 @@ class HTTPClient(threading.Thread):
 		self.hostname = hostname
 
 	def run(self):
-		global def_password, http_exit
+		global def_password, http_exit, SPLUNK, splunk_index, local_hostname
 		username = self.username
 		hostname = self.hostname
 		url = 'http://' + hostname
 		while http_exit.is_set() == False:			
 			try:
 				hrsp = requests.get(url, auth=(username, def_password), timeout=(1,5))
-				drslt = subprocess.check_output('dig +short ' + hostname, shell=True, text=True)
+				try:
+					drslt = subprocess.check_output('dig +short ' + hostname, shell=True).decode('utf-8')
+				except:
+					pass
 				drslt_parts = drslt.split('\n')
 				rmt_ip = 'Unknown'
 				if len(drslt_parts) > 1:
@@ -194,6 +242,15 @@ class HTTPClient(threading.Thread):
 						else:
 							rmt_ip = drslt_parts[0]
 
+				if SPLUNK:
+					event = str(time.time()) + ','
+					event += 'protocol="HTTP",'
+					event += 'ip_address=' + rmt_ip + ','
+					event += 'honey_hostname="' + hostname + '",'
+					event += 'honey_username="' + username + '",'
+					event += 'message="Responder activity detected!"'
+					splunk_index.submit(event, sourcetype="honeycreds.service", host=local_hostname)
+
 				logging.critical(messages.RSP_RECVD % {'color':'', 'bold':'', 'proto':'HTTP', 'end':'', 'ip':rmt_ip, 'hostname':hostname})
 				print(messages.RSP_RECVD % {'color':termcolor.BLUE, 'bold':termcolor.BOLD, 'proto':'HTTP', 'end':termcolor.END, 'ip':rmt_ip, 'hostname':hostname})
 			except:
@@ -202,7 +259,7 @@ class HTTPClient(threading.Thread):
 				http_exit.wait(HTTP_SLEEP)
 
 def main():
-	global smb_Thread, http_Thread
+	global smb_Thread, http_Thread, SPLUNK
 	os.system('clear')
 	banner()
 	print(termcolor.GREEN + termcolor.BOLD + '[+]' + termcolor.END + ' Sending events...')
@@ -214,6 +271,8 @@ def main():
 	if str.upper(HTTP) == 'ON':
 		http_Thread = HTTPClient(username, hostname)
 		http_Thread.start()
+	if SPLUNK:
+		init_splunk()
 
 if __name__ == '__main__':
 	signal(SIGINT, signal_handler)
